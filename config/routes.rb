58f2b5fec7b33b18e799d67698f152e4ed2ba29f
mitragroups <miftahmuhajir@gmail.com>
2023-08-31 03:26:40 +0700

    Add Resources in Routes for Authors

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'authors/index'
   resources :books
   get 'demo/index'
+  resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
