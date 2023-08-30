52a6817aaf5bcc07afdcaa35dec98cf524b30d2f
mitragroups <miftahmuhajir@gmail.com>
2023-08-31 03:30:18 +0700

    Migration is Successful for Authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_145849) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_202139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name", limit: 100, default: "Belum ada nama"
+    t.integer "age", default: 0
+    t.string "gender"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
