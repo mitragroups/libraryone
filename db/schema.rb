5383ce5e396365f4b644c893d1a4ebeb447ed249
mitragroups <miftahmuhajir@gmail.com>
2023-10-23 13:04:16 +0700

    Add Migration AuthorIdToBooks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_202139) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_055939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_202139) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
   end
 
 end
