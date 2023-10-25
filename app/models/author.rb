bc0efeaf26bbaf093cd476a9120427b8357c1656
mitragroups <miftahmuhajir@gmail.com>
2023-10-25 16:46:18 +0700

    Add urutan_usia

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ApplicationRecord
     def self.author_male
         where("gender != 'Female'").pluck(:name)
     end
+    
+    def self.urutan_usia 
+        order(age: :asc)
+    end
 end
