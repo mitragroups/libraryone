d37ccd2b77a7c9f009e5ce62b1dc6389d416253a
mitragroups <miftahmuhajir@gmail.com>
2023-11-13 15:17:38 +0700

    Clear Method in Book Model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,23 +1,4 @@
 class Book < ApplicationRecord
     belongs_to :author 
-    
-    def self.lebihmurah
-        where('price < 200000')        
-    end
 
-    def self.equal_more(price)
-        where('price >= ?', price)
-    end
-
-    def self.book_title 
-        pluck(:title)
-    end
-
-    def self.ror 
-        where(title: 'Buku Ruby on Rails').pluck(:price)
-    end
-
-    def self.buku_termahal 
-        order(price: :asc).pluck(:title, :price, :page)
-    end
 end
